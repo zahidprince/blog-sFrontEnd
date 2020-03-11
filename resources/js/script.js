514d248ec3e5610c758f4047aef2d70cb3970079
Zahidul Islam Prince <zahidulisprince@gmail.com>
2020-03-11 12:44:14 +0600

    Mobile Nav Animation Changed

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,20 +1,31 @@
 $(document).ready(function() {
     
+
     $("#js-mobile-nav").on('click', function(e){
         e.preventDefault();
-        
-        $("#menu").fadeIn( "slow", function() {
 
-        });
-    });
+        $("#menu").animate({
+            opacity: 'show',
+            height: 'show',
+            marginTop: 'show',
+            marginBottom: 'show',
+            paddingTop: 'show',
+            paddingBottom: 'show'
+        })
+    })
 
     $("#menuClose").on('click', function(e){
         e.preventDefault();
-        
-        $("#menu").fadeOut( "slow", function() {
-            console.log("com");
-        });
-    });
+
+        $("#menu").animate({
+            opacity: 'hide',
+            height: 'hide',
+            marginTop: 'hide',
+            marginBottom: 'hide',
+            paddingTop: 'hide',
+            paddingBottom: 'hide'
+        })
+    })
 
     
 
